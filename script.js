cf40dc1bb6bfaa1bd82d5aa7ab14f04a9b792766
Androm332 <g23456789korolyov@gmail.com>
2025-02-03 22:38:00 +0300

    s

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.querySelector(".get-code-button"); // Проверь класс кнопки
-    if (button) {
-        button.remove();
-    }
-});
